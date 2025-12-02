2223342e353f541844657aeea2efdd396824f968
Maddox Guthrie <m4dpup@gmail.com>
2025-12-02 13:38:27 -0600

    add part two

--- day_1/secret_entrance.cpp
+++ day_1/secret_entrance.cpp
@@ -2,17 +2,10 @@
 #include <fstream>
 #include <string>
 
-int main(int argc, char **argv) {
-    if(argc < 2) {
-        std::cerr << "No input file provided\n";
-        return -1;
-    }
-
-    std::ifstream codes(argv[1]);
+void partOne(std::ifstream *codes) {
     int direction = 1, amount = 0, position = 50, counter = 0;
     std::string movement;
-    while(std::getline(codes, movement)) {
-        std::cout << position << std::endl;
+    while(std::getline(*codes, movement)) {
         if(movement.empty()) break;
         direction = movement.at(0) == 'L' ? -1 : 1;
         amount = std::stoi(movement.substr(1, movement.length()));
@@ -21,5 +14,38 @@ int main(int argc, char **argv) {
     }
 
     std::cout << "The dial stops on 0: " << counter << " times" << std::endl;
+}
+
+void partTwo(std::ifstream *codes) {
+    int direction = 1, amount = 0, position = 50, counter = 0;
+    std::string movement;
+
+    while(std::getline(*codes, movement)) {
+        if(movement.empty()) break;
+        direction = movement.at(0) == 'L' ? -1 : 1;
+        amount = std::stoi(movement.substr(1, movement.length()));
+        for(int i = 0; i < amount; i++) {
+            position = (position + direction) % 100;
+            if(!position) counter++;
+        }
+    }
+
+    std::cout << "The dial passes or stops on 0: " << counter << " times" << std::endl;
+}
+
+int main(int argc, char **argv) {
+    if(argc < 2) {
+        std::cerr << "No input file provided\n";
+        return -1;
+    }
+
+    std::ifstream codes(argv[1]);
+
+    partOne(&codes);
+    codes.clear();
+    codes.seekg(0, std::ios::beg);
+    partTwo(&codes);
+
     codes.close();
+    return 0;
 }
