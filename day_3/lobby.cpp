2491387dfc58ddb85412b854ccc34296d47fcf9d
Maddox Guthrie <m4dpup@gmail.com>
2025-12-15 11:08:18 -0600

    Add day 3 part one

--- day_3/lobby.cpp
+++ day_3/lobby.cpp
@@ -1,48 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 void partOne(std::ifstream *banks) {
+    std::vector<int> batteries;
     int total = 0;
     std::string bank;
     while(std::getline(*banks, bank)) {
-        int bankSize = bank.length();
-        int highest = 0, highestIndex = 0;
-
-        for(int i = 0; i < bankSize; i++) {
-            int battery = (bank[i] - '0');
-            if(battery > highest) {
-                highest = battery;
-                highestIndex = i;
+        for(int i = 0; i < bank.length(); i++) {
+            for(int j = i + 1; j < bank.length(); j++) {
+                batteries.push_back(((bank[i] - '0') * 10) + (bank[j] - '0'));
             }
         }
 
-        int secondHighest = 0;
-        if(bankSize - 1 == highestIndex) {
-            for(int i = 0; i < bankSize - 1; i++) {
-                int battery = (bank[i] - '0');
-                if(battery > secondHighest) {
-                    secondHighest = battery;
-                }
-            }
-
-            total += (secondHighest * 10) + (highest);
-            std::cout << (secondHighest * 10) + (highest) << std::endl;
-        } else {
-            for(int i = highestIndex + 1; i < bankSize - 1; i++) {
-                int battery = (bank[i] - '0');
-                if(battery > secondHighest) {
-                    secondHighest = battery;
-                }
-            }
-
-             total += (highest * 10) + (secondHighest);
-             std::cout << (highest * 10) + (secondHighest) << std::endl;
-        }
+        std::sort(batteries.begin(), batteries.end());
+        total += batteries.back();
+        batteries.clear();
     }
 
     std::cout << "The total output joltage is: " << total << " jolts." << std::endl;
-
 }
 
 int main(int argc, char **argv) {
