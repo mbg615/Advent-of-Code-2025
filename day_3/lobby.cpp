df1295736ad19a7cde9dec001806d6ac8844b3df
Maddox Guthrie <m4dpup@gmail.com>
2025-12-18 12:46:59 -0600

    Add day 3 part 2 solution, and update .gitignore

--- day_3/lobby.cpp
+++ day_3/lobby.cpp
@@ -22,6 +22,40 @@ void partOne(std::ifstream *banks) {
     std::cout << "The total output joltage is: " << total << " jolts." << std::endl;
 }
 
+void partTwo(std::ifstream *banks) {
+    std::vector<int> batteries;
+    long total = 0;
+    std::string bank;
+    while(std::getline(*banks, bank)) {
+        for(std::string::iterator it = bank.begin(); it != bank.end(); it++) {
+            size_t remaining = std::distance(it, bank.end());
+            int digit = *it - '0';
+
+            while(!batteries.empty() && digit > batteries.back() && batteries.size() + remaining > 12) {
+                batteries.pop_back();
+            }
+
+            if(batteries.size() < 12) {
+                batteries.push_back(digit);
+            }
+        }
+
+        total += [stack = &batteries]() -> long {
+            long x = 0;
+            for(int &e : *stack) {
+                x += e;
+                x *= 10;
+            }
+            return x /= 10;
+        }();
+
+        batteries.clear();
+    }
+
+    std::cout << "The total output joltage is: " << total << " jolts." << std::endl;
+}
+
+
 int main(int argc, char **argv) {
     if(argc < 2) {
         std::cerr << "No input file provided\n";
@@ -31,5 +65,10 @@ int main(int argc, char **argv) {
     std::ifstream banks(argv[1]);
 
     partOne(&banks);
+    banks.clear();
+    banks.seekg(0, std::ios::beg);
+    partTwo(&banks);
 
+    banks.close();
+    return 0;
 }
